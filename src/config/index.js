f9d6db7cb0d639a6910a570186eb23626120f51a
Afroz Middya <160333462+afrozmiddya@users.noreply.github.com>
2026-03-11 11:16:27 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@
 // export const BACKEND_URL = "http://localhost:5000";
 
 // Production backend
-export const BACKEND_URL = "anikabackend-main-production.up.railway.app";
+export const BACKEND_URL = "anika-backend-main.railway.internal";
 
 console.log("Using backend URL:", BACKEND_URL);
