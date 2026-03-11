6a1c674dad3fe61db9c54cec4436c454dab75cbd
Afroz Middya <160333462+afrozmiddya@users.noreply.github.com>
2026-03-11 11:14:26 +0530

    Update backend URL for production environment

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@
 // export const BACKEND_URL = "http://localhost:5000";
 
 // Production backend
-export const BACKEND_URL = "https://053c404a.anika-backend-main.pages.dev/";
+export const BACKEND_URL = "anikabackend-main-production.up.railway.app";
 
 console.log("Using backend URL:", BACKEND_URL);
